6625655d9c4c632da9109f7a5a59fe239ae7909c
nyf <nsotacuromatos46@gmail.com>
2021-03-03 15:57:18 -0500

    pagina web hasta contactos

--- php/correo.php
+++ php/correo.php
@@ -1,5 +1,5 @@
 <?php
-$destinatario = 'correo@gmail.com';
+$destinatario = 'heartring13@gmail.com';
 //correo que se enviara 
 $nombre = $_POST['nombre'];
 $asunto = $_POST['asunto'];
@@ -9,8 +9,9 @@ $email = $_POST['email'];
 $header = "Enviado desde la pagina de la empresa GALYSAN SAC";
 $mensajeCompleto = $mensaje. "\nAtentamente: " . $nombre;
 mail($destinatario, $asunto, $mensajeCompleto, $header);
-echo "<script>alert('correo enviado exitosamente') </script>";
-echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+echo "<script> alert('correo enviado exitosamente') </script>";
+echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+
  
 
 ?>
